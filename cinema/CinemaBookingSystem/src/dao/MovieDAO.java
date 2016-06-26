6ffa696f7b49c186b87a85bf8c2383f1007ee25a
MilicaB <milicabb13@gmail.com>
2016-06-26 18:33:07 +0300

    Adding DAOs

---
+++ cinema/CinemaBookingSystem/src/dao/MovieDAO.java
@@ -0,0 +1,48 @@
+package dao;
+
+import java.util.Date;
+import java.util.List;
+
+import javax.ejb.Singleton;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+
+import model.Movie;
+
+
+@Singleton
+public class MovieDAO {
+
+    @PersistenceContext
+    private EntityManager em;
+    
+    public void addMovie(Movie movie) {
+        em.persist(movie);
+        em.flush();
+    }
+
+        
+    public List<Movie> getAll() {
+        String txtQuery = "SELECT m FROM Movie m";
+        TypedQuery<Movie> query = em.createQuery(txtQuery, Movie.class);
+        return query.getResultList();
+    }
+    
+    public List<Movie> getMoviesAfter(Date startingTime) {
+        String txtQuery = "SELECT m FROM Movie m WHERE m.startingTime>=:startingTime";
+        TypedQuery<Movie> query = em.createQuery(txtQuery, Movie.class);
+        query.setParameter("date", startingTime);
+        return query.getResultList();
+    }
+    
+    public Movie findMoviebyName(String moviename)
+    {
+        String txtQuery = "Select m FROM Movie m WHERE m.movieName=:moviename";
+        TypedQuery<Movie>query = em.createQuery(txtQuery,Movie.class);
+        query.setParameter("moviename", moviename);
+        return query.getSingleResult();
+    }
+
+}
+
